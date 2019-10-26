382ac8b0a47fde179610a496eea68740328bb414
ErykUrban <eryk55555555@gmail.com>
2019-10-26 23:49:01 +0200

    nowe

---
+++ 01-TypesAndVariables/Zadania/cm na stopy i cale.py
@@ -0,0 +1,6 @@
+wzrost = 178
+cal = 2.54
+stopa = 30.48
+wzrost_stopa = wzrost//stopa
+wzrost_cal = (wzrost-(wzrost_stopa*stopa))//cal
+print("Mam " + str(wzrost) + "cm wzrostu, tj. " + str(wzrost_stopa) +" stóp i " +str(wzrost_cal) + " cali.")
