4efbf69ddbdcaea0fe859a28bd4f627dad6fb9f2
ErykUrban <eryk55555555@gmail.com>
2019-10-26 17:36:43 +0200

    Lekcja 1

---
+++ 00-CourseIntroduction/MyPersonalData.py
@@ -0,0 +1,4 @@
+wiek = 19
+imię = "Eryk"
+nazwisko = "Urban"
+print( imię + " " + nazwisko + " " + str(wiek))
--- 00-CourseIntroduction/Program.py
+++
@@ -1,7 +0,0 @@
-# Imię/Nazwisko/Wiek
-name='Eryk'
-surname='Urban'
-Age='19'
-print( "Hi" )
-print( name + surname )
-print( Age )
